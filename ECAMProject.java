fbb2f6a39fb3c0bd2cdbd4e89c5e95fd9435ed04
JDyson4 <63075765+JDyson4@users.noreply.github.com>
2021-01-21 17:14:48 -0500

    Add files via upload

---
+++ ECAMProject.java
@@ -0,0 +1,22 @@
+/**
+ * IST 261/361
+ * ECAMProject.java
+ * 
+ * Purpose: 
+ */
+package ecam.project;
+
+/**
+ *
+ * @author jpdys
+ */
+public class ECAMProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
