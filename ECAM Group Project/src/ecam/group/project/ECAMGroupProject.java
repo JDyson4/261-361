607c6afd794a8a6b31a93f81865713e8d5de1166
jpdys <jpdys@DESKTOP-BBFRM9U>
2021-01-21 17:24:24 -0500

    Added comments

---
+++ ECAM Group Project/src/ecam/group/project/ECAMGroupProject.java
@@ -0,0 +1,21 @@
+/**
+ * IST 261/361
+ * ECAMGroupProject.java
+ * Purpose: 
+ */
+package ecam.group.project;
+
+/**
+ *
+ * @author jpdys
+ */
+public class ECAMGroupProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
