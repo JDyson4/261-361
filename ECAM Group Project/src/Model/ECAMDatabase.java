ddc9621de887c0ee403f4d782a4024ca7101468c
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-02-22 15:50:14 -0500

    Database class created -River

--- ECAM Group Project/src/Model/ECAMDatabase.java
+++ ECAM Group Project/src/Model/ECAMDatabase.java
@@ -15,6 +15,45 @@ import java.sql.Statement;
  * @author RIVER
  */
 public class ECAMDatabase {
-    
-   
+     public ECAMDatabase () {
+        
+        String className = "org.apache.derby.jdbc.ClientDriver";
+        String url = "jdbc:derby://192.168.1.62:1527/ECAMProjectDB";
+        String user = "app";
+        String password = "app";
+
+        try
+        {
+            Class.forName(className);
+        }
+        catch (Exception e)
+        {
+            System.out.println(className + " driver failed to load.");
+            System.exit(-1);
+        }
+        try
+        {
+            System.out.println("We made it");
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * from Inventory ORDER BY ProductName");
+
+            while (rs.next())
+            {
+                String product = rs.getString("");
+                String description = rs.getString("ProductDescription");
+
+                float price = rs.getFloat("Price");
+
+                System.out.println(product + " described by many as \"" + description +
+                        "\" is sold for $" + price);
+            }
+            stmt.close();
+            con.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 }
