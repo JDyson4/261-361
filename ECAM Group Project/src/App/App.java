93d49f5e4e09a6c88a7a64b69d0a39a2295ee35a
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-03-14 17:21:08 -0400

    App class

---
+++ ECAM Group Project/src/App/App.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App;
+
+import Controller.ECAMController;
+import Model.ECAMModel;
+import View.ECAMView;
+
+/**
+ *
+ * @author RIVER
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ECAMModel model = new ECAMModel();
+        ECAMView view = new ECAMView();
+        ECAMController controller = new ECAMController(model, view);
+    }
+    
+}
