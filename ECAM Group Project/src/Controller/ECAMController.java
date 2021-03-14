f88c7da13dbc262ef6908f054da3932a38d6ca16
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-03-14 18:59:50 -0400

    Controller class update

--- ECAM Group Project/src/Controller/ECAMController.java
+++ ECAM Group Project/src/Controller/ECAMController.java
@@ -6,28 +6,200 @@
 package Controller;
 
 import Model.ECAMModel;
+import View.ECAMMainFrame;
 import View.ECAMView;
-import java.util.*;
-import java.io.*;
-import java.sql.*;
+import java.awt.event.*;
 /**
  *
  * @author jpdys
  */
 public class ECAMController {
     //
-    ECAMModel model;
-    ECAMView view;
+    private ECAMModel model;
+    private ECAMView view;
     
     public ECAMController(ECAMModel model, ECAMView view) {
         this.model = model;
         this.view = view;
-    }
-    
-    public static void main(String[] args) {
-        ECAMModel model = new ECAMModel();
-        ECAMView view = new ECAMView();
-        ECAMController controller = new ECAMController(model, view);
-        System.out.println("River was here on March 14th");
-    }
+        
+        view.getLogin().getjLabel1().addMouseListener(
+            new MouseAdapter()
+            {
+                @Override
+                public void mouseClicked(MouseEvent evt)
+                {
+                    System.exit(0);
+                }
+            }
+        );//end JLabel1 end MouseListener
+        
+        view.getLogin().getUsername().addFocusListener(
+            new FocusAdapter()
+            {
+                @Override
+                public void focusLost(FocusEvent evt)
+                {
+                    if ("".equals(view.getLogin().getUsername().getText())) {
+                        view.getLogin().getUsername().setText("Username");
+                    }
+                }
+            }
+        );//end Username FocusListener
+        
+        view.getLogin().getPassword().addFocusListener(
+            new FocusAdapter()
+            {
+                @Override
+                public void focusLost(FocusEvent evt)
+                {
+                    if (view.getLogin().getPassword().getPassword().length == 0) {
+                        view.getLogin().getPassword().setText("Password");
+                    }
+                }
+            }
+        );//end Password FocusListener
+        
+        view.getLogin().getUsername().addMouseListener(
+            new MouseAdapter()
+            {
+                @Override
+                public void mouseClicked(MouseEvent evt)
+                {
+                    //"Password" is the text in the password field then text in password field text is revealed
+                    //Text is hidden if text in the password field does not equal "Password"
+                    if ("Password".equals(String.valueOf(view.getLogin().getPassword().getPassword()))){
+                        view.getLogin().getPassword().setEchoChar((char) 0);
+                    }
+                    //if "Username" is the text in the username field then username field text is set blank
+                    if ("Username".equals(view.getLogin().getUsername().getText())){
+                        view.getLogin().getUsername().setText("");
+                    }
+                }
+            }
+        );//end Username MouseListener
+        
+        view.getLogin().getPassword().addMouseListener(
+            new MouseAdapter()
+            {
+                @Override
+                public void mouseClicked(MouseEvent evt)
+                {
+                    if ("Password".equals(String.valueOf(view.getLogin().getPassword().getPassword()))){
+                        view.getLogin().getPassword().setText("");
+                        view.getLogin().getPassword().setEchoChar((char)0x2022); //this will display '•' for every character in the password
+                    }
+                }
+            }
+        );//end Password MouseListener
+        
+        view.getLogin().getSignIn().addActionListener(
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String userID = view.getLogin().getUsername().getText();
+                    String passcode = String.valueOf(view.getLogin().getPassword().getPassword());
+        
+                    if(view.getLogin().getUserInfo().containsKey(userID)) {
+                        if (view.getLogin().getUserInfo().get(userID).equals(passcode)) {
+                            view.getLogin().dispose();
+                            ECAMMainFrame mf = new ECAMMainFrame();
+                            mf.getInitialPanel().getjButton1().addActionListener(
+                                    new ActionListener()
+                                    {
+                                        @Override
+                                        public void actionPerformed(ActionEvent e)
+                                        {
+                                            mf.getInitialPanel().getjLabel1().setText("River Was Here");
+                                        }
+                                    }    
+                            );
+                        } else {
+                            view.getLogin().getjLabel3().setText("Password is incorrect");
+                        }
+                    } else {
+                        view.getLogin().getjLabel3().setText("Username does not exist");
+                    }
+                }
+            }
+        );//end SignIn ActionListener
+        
+        view.getLogin().getUsername().addKeyListener(
+            new KeyAdapter()
+            {
+                @Override
+                public void keyPressed(KeyEvent evt)
+                {
+                    if ("Username".equals(view.getLogin().getUsername().getText())){
+                        view.getLogin().getUsername().setText("");
+                    }
+                    if(evt.getKeyCode() == KeyEvent.VK_ENTER) {
+                        String userID = view.getLogin().getUsername().getText();
+                        String passcode = String.valueOf(view.getLogin().getPassword().getPassword());
+
+                        if(view.getLogin().getUserInfo().containsKey(userID)) {
+                            if (view.getLogin().getUserInfo().get(userID).equals(passcode)) {
+                                view.getLogin().dispose();
+                                ECAMMainFrame mf = new ECAMMainFrame();
+                                mf.getInitialPanel().getjButton1().addActionListener(
+                                    new ActionListener()
+                                    {
+                                        @Override
+                                        public void actionPerformed(ActionEvent e)
+                                        {
+                                            mf.getInitialPanel().getjLabel1().setText("River Was Here");
+                                        }
+                                    }    
+                                );
+                            } else {
+                                view.getLogin().getjLabel3().setText("Password is incorrect");
+                            }
+                        } else {
+                            view.getLogin().getjLabel3().setText("Username does not exist");
+                        }
+                    }
+                }
+            }
+        );//end Username KeyListener
+        
+        view.getLogin().getPassword().addKeyListener(
+            new KeyAdapter()
+            {
+                @Override
+                public void keyPressed(KeyEvent evt)
+                {
+                    if ("Password".equals(String.valueOf(view.getLogin().getPassword().getPassword()))){
+                        view.getLogin().getPassword().setText("");
+                        view.getLogin().getPassword().setEchoChar((char)0x2022); //this will display '•' for every character in the password
+                    }
+                    if(evt.getKeyCode() == KeyEvent.VK_ENTER) {
+                        String userID = view.getLogin().getUsername().getText();
+                        String passcode = String.valueOf(view.getLogin().getPassword().getPassword());
+
+                        if(view.getLogin().getUserInfo().containsKey(userID)) {
+                            if (view.getLogin().getUserInfo().get(userID).equals(passcode)) {
+                                view.getLogin().dispose();
+                                ECAMMainFrame mf = new ECAMMainFrame();
+                                mf.getInitialPanel().getjButton1().addActionListener(
+                                    new ActionListener()
+                                    {
+                                        @Override
+                                        public void actionPerformed(ActionEvent e)
+                                        {
+                                            mf.getInitialPanel().getjLabel1().setText("River Was Here");
+                                        }
+                                    }    
+                                );
+                            } else {
+                                view.getLogin().getjLabel3().setText("Password is incorrect");
+                            }
+                        } else {
+                            view.getLogin().getjLabel3().setText("Username does not exist");
+                        }
+                    }
+                }
+            }
+        );//end Password KeyListener
+        
+    }//end Controller Constructor
 }
