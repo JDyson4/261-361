b43560025d658d9d3a6eb3120d771713e3bdf065
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-03-24 12:52:43 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/App/App.java
+++ ECAM Group Project/src/App/App.java
@@ -6,9 +6,11 @@
 package App;
 
 import Controller.ECAMController;
+import Model.ECAMDatabase;
 import Model.ECAMModel;
 import View.ECAMView;
 import java.awt.*;
+import java.sql.SQLException;
 import javax.swing.*;
 
 /**
@@ -20,10 +22,12 @@ public class App {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        ECAMModel model = new ECAMModel();
-        ECAMView view = new ECAMView();
-        ECAMController controller = new ECAMController(model, view);
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        //ECAMModel model = new ECAMModel();
+        //ECAMView view = new ECAMView();
+        //ECAMController controller = new ECAMController(model, view);
+        ECAMDatabase db = new ECAMDatabase();
+        System.out.println(db.getUsername());
     }
     
 }
