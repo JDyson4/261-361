7cb70a95d1d38ff398db776845afe3f613eec984
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-03-28 02:45:04 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/Model/ECAMDatabase.java
+++ ECAM Group Project/src/Model/ECAMDatabase.java
@@ -5,6 +5,7 @@
  */
 package Model;
 
+import java.awt.Image;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,7 +14,9 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
 
 public class ECAMDatabase {
     
@@ -39,18 +42,37 @@ public class ECAMDatabase {
         return con;
     }
     
-    public void getUsername() throws SQLException, ClassNotFoundException {
+    /**
+     * TESTING CODE FOR DATETIME FORMATTING
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public void getOrders() throws SQLException, ClassNotFoundException {
         Connection con = getConnection();
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM aircraft");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM orders");
         
-        String programNo = "";
-        String programName = "";
+        String orderNo = "";
+        String customerNo = "";
+        String orderDate = "";
         while(rs.next()){
-            programNo = rs.getString("programNo");
-            programName = rs.getString("programName");
-            System.out.println(programNo + " " + programName);
+            orderNo = rs.getString("orderNo");
+            customerNo = rs.getString("customerNo");
+            orderDate = rs.getString("orderDate");
+            System.out.println(orderNo + " " + customerNo + " " + orderDate.substring(0, orderDate.length() - 2));
         }
+    }
+    
+    public byte[] getDrawing() throws SQLException, ClassNotFoundException {
+        Connection con = getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT drawing FROM drawings WHERE drawingNo = 1");
+        
+        byte[] drawing = "hello".getBytes();
+        while(rs.next()){
+            drawing = rs.getBytes("drawing");
+        }
+        return drawing;
     }
     
     public ArrayList<CustomerPrograms> retrieveCustomerProgramsReport() throws SQLException {
@@ -206,21 +228,84 @@ public class ECAMDatabase {
         return engineerEngineerDrawings;
     }
     
-    public ArrayList<EngineerDrawingChanges> retrieveEngineerDrawingChangesReport() throws SQLException {
-        ArrayList<EngineerDrawingChanges> engineerDrawingChanges = new ArrayList<EngineerDrawingChanges>();
+    /**
+     * Retrieves a report of all status changes made to a specific engineering drawing, 
+     * including the date the change was made and engineer who made it.
+     * 
+     * @return
+     * @throws SQLException 
+     */
+    public AbstractTableModel retrieveEngineerDrawingChangesReport() throws SQLException {
         
         Connection con = getConnection();
         Statement stmt;
         ResultSet rs;
         
         stmt = con.createStatement();
-        rs = stmt.executeQuery("SELECT * FROM CUSTOMER");
+        rs = stmt.executeQuery("SELECT drawings.DrawingNo, "
+                                    + "drawings.Drawing, "
+                                    + "drawings.Version, "
+                                    + "drawings.VersionDateTime, "
+                                    + "drawings.ReasonForChange, "
+                                    + "employees.EmployeeNo, "
+                                    + "employees.EmployeeFName, "
+                                    + "employees.EmployeeLName "
+                             + "FROM drawings "
+                             + "JOIN engineerdrawings "
+                                + "ON drawings.DrawingNo = engineerdrawings.DrawingNo "
+                             + "JOIN employees "
+                                + "ON employees.EmployeeNo = engineerdrawings.EmployeeNo");
         
+        ArrayList<EngineerDrawingChanges> edcA = new ArrayList<EngineerDrawingChanges>();
+        EngineerDrawingChanges edc;
         while(rs.next()){
-            
+            String versionDT = rs.getString("drawings.VersionDateTime");
+            edc = new EngineerDrawingChanges(
+                rs.getInt("drawings.DrawingNo"),
+                rs.getBytes("drawings.Drawing"),
+                rs.getDouble("drawings.Version"),
+                versionDT.substring(0, versionDT.length() - 2),
+                rs.getString("drawings.ReasonForChange"),
+                rs.getInt("employees.EmployeeNo"),
+                rs.getString("employees.EmployeeFName"),
+                rs.getString("employees.EmployeeFName")
+            );
+            edcA.add(edc);
         }
         
-        return engineerDrawingChanges;
+        Object[][] edcRows = new Object[edcA.size()][8];
+        
+        for(int i = 0; i < edcA.size(); i++)
+        {
+            edcRows[i][0] = edcA.get(i).getDrawingNo();
+            if (edcA.get(i).getDrawing() != null) {
+                ImageIcon drawing = new ImageIcon(
+                        new ImageIcon(edcA.get(i).getDrawing()).getImage()
+                                .getScaledInstance(150, 120, Image.SCALE_SMOOTH));
+                edcRows[i][1] = drawing;
+            } else {
+                edcRows[i][1] = null;
+            }
+            edcRows[i][2] = edcA.get(i).getVersion();
+            edcRows[i][3] = edcA.get(i).getVersionDateTime();
+            edcRows[i][4] = edcA.get(i).getReasonForChange();
+            edcRows[i][5] = edcA.get(i).getEmployeeNo();
+            edcRows[i][6] = edcA.get(i).getEmployeeFName();
+            edcRows[i][7] = edcA.get(i).getEmployeeLName();
+        }
+        
+        String[] edcColumnNames = {"Drawing No.","Drawing","Version",
+                                "Version DateTime", "Reason For Change", 
+                                "Employee No.", "Employee FName", "Employee LName"};
+        
+        DrawingChangesReportTableModel dcrtm = new DrawingChangesReportTableModel(edcColumnNames,edcRows){
+            @Override
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        };
+        
+        return dcrtm;
     }
     
     public void insertRowCustomerProgramsReport(JTextField value) throws SQLException {
