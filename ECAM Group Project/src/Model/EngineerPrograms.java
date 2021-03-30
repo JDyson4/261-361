2ef07edaf70cea2a3f4ab795568850ea79ce4440
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-03-29 23:38:55 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/Model/EngineerPrograms.java
+++ ECAM Group Project/src/Model/EngineerPrograms.java
@@ -14,13 +14,17 @@ public class EngineerPrograms {
     private int employeeNo;
     private String employeeFName;
     private String employeeLName;
+    private String skill;
+    private String jobTitle;
     private int programNo;
     private String programName;
     
-    public EngineerPrograms(int eN, String eFN, String eLN, int pNo, String pNa) {
+    public EngineerPrograms(int eN, String eFN, String eLN, String s, String jT, int pNo, String pNa) {
         this.employeeNo = eN;
         this.employeeFName = eFN;
         this.employeeLName = eLN;
+        this.skill = s;
+        this.jobTitle = jT;
         this.programNo = pNo;
         this.programName = pNa;
     }
@@ -93,5 +97,33 @@ public class EngineerPrograms {
      */
     public void setProgramName(String programName) {
         this.programName = programName;
+    }
+
+    /**
+     * @return the JobTitle
+     */
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    /**
+     * @param jobTitle the jobTitle to set
+     */
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    /**
+     * @return the skill
+     */
+    public String getSkill() {
+        return skill;
+    }
+
+    /**
+     * @param skill the skill to set
+     */
+    public void setSkill(String skill) {
+        this.skill = skill;
     }
 }
