4f3260ca109f893f78f3185a6e7cc228ce911a03
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-03-29 23:37:57 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/Model/EngineerDrawingChanges.java
+++ ECAM Group Project/src/Model/EngineerDrawingChanges.java
@@ -19,8 +19,10 @@ public class EngineerDrawingChanges {
     private int employeeNo;
     private String employeeFName;
     private String employeeLName;
+    private String skill;
+    private String jobTitle;
     
-    public EngineerDrawingChanges(int dNO, byte[] d, double v, String vDT, String rFC, int eN, String eFN, String eLN) {
+    public EngineerDrawingChanges(int dNO, byte[] d, double v, String vDT, String rFC, int eN, String eFN, String eLN, String s, String jT) {
         this.drawingNo = dNO;
         this.drawing = d;
         this.version = v;
@@ -29,6 +31,8 @@ public class EngineerDrawingChanges {
         this.employeeNo = eN;
         this.employeeFName = eFN;
         this.employeeLName = eLN;
+        this.skill = s;
+        this.jobTitle = jT;
     }
 
     /**
@@ -141,5 +145,33 @@ public class EngineerDrawingChanges {
      */
     public void setEmployeeLName(String employeeLName) {
         this.employeeLName = employeeLName;
+    }
+
+    /**
+     * @return the jobTitle
+     */
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    /**
+     * @param jobTitle the jobTitle to set
+     */
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    /**
+     * @return the skill
+     */
+    public String getSkill() {
+        return skill;
+    }
+
+    /**
+     * @param skill the skill to set
+     */
+    public void setSkill(String skill) {
+        this.skill = skill;
     }
 }
