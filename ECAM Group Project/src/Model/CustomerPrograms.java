e077ac93e63da5dc46b92fa2d3c3b0a9bcb720f5
jpdys <jpdys@DESKTOP-BBFRM9U>
2021-04-02 22:23:21 -0400

    Added some required attributes and methods

--- ECAM Group Project/src/Model/CustomerPrograms.java
+++ ECAM Group Project/src/Model/CustomerPrograms.java
@@ -5,8 +5,43 @@ package Model;
 
 /**
  *
- * @author jpdys
+ * @author John
  */
 public class CustomerPrograms {
+    private int programNo;
+    private int customerNo;
+    
+    public CustomerPrograms(int prNo, int custNo) {
+        programNo = prNo;
+        customerNo = custNo;
+    }
+
+    /**
+     * @return the programNo
+     */
+    public int getProgramNo() {
+        return programNo;
+    }
+
+    /**
+     * @param programNo the programNo to set
+     */
+    public void setProgramNo(int programNo) {
+        this.programNo = programNo;
+    }
+
+    /**
+     * @return the customerNo
+     */
+    public int getCustomerNo() {
+        return customerNo;
+    }
+
+    /**
+     * @param customerNo the customerNo to set
+     */
+    public void setCustomerNo(int customerNo) {
+        this.customerNo = customerNo;
+    }
     
 }
