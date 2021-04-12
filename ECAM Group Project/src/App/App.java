9940cd62a52dd3b0a277f94dac4a2823dd2016ef
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-04-12 01:00:14 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/App/App.java
+++ ECAM Group Project/src/App/App.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package App;
 
 import Controller.ECAMController;
@@ -11,8 +6,7 @@ import View.ECAMView;
 import java.sql.SQLException;
 
 /**
- *
- * @author RIVER
+ * Purpose: Runs MVC structured application
  */
 public class App {
 
