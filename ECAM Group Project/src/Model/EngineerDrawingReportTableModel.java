ad229d8bb36773617b85c9a24cf81d1bc4c8eadc
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-04-12 01:03:52 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/Model/EngineerDrawingReportTableModel.java
+++ ECAM Group Project/src/Model/EngineerDrawingReportTableModel.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 import javax.swing.Icon;
 import javax.swing.table.AbstractTableModel;
 
 /**
- *
- * @author RIVER
+ * Purpose: Custom AbstractTableModel for Engineer to Drawing report
  */
 public class EngineerDrawingReportTableModel extends AbstractTableModel {
 
