f08484feb02958d06b47ad5d8bd9571699add868
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-04-12 01:06:54 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/View/ECAMLoginFrame.java
+++ ECAM Group Project/src/View/ECAMLoginFrame.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
 import javax.swing.ImageIcon;
 
 /**
- *
- * @author RIVER
+ * Used to display login form to the computer screen
  */
 public class ECAMLoginFrame extends javax.swing.JFrame {
     
