dc3301d4173a42de1bdb4c3812cdbf7f8bc17d71
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-04-12 01:01:07 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/Model/AircraftParts.java
+++ ECAM Group Project/src/Model/AircraftParts.java
@@ -1,69 +1,92 @@
-/**
- * 
- */
 package Model;
 
 /**
- *
- * @author Catie
+ * Purpose: Object to store one row of Aircraft to Parts report
  */
 public class AircraftParts {
     
-    private int programNo,
-                partNo,
-                partInvent;
-    private String programName,
-                   partVendor;
+    private int programNo;
+    private String programName;
+    private int partNo;
+    private int inventory;
+    private String vendor;
 
-    public AircraftParts() {
+    public AircraftParts(int progNo, String progName, int pNo, int inv, String ven) {
+        programNo = progNo;
+        partNo = pNo;
+        inventory = inv;
+        programName = progName;
+        vendor = ven;
     }
 
-    public AircraftParts(int programNo, int partNo, int partInvent, String programName, String partVendor) {
-        this.programNo = programNo;
-        this.partNo = partNo;
-        this.partInvent = partInvent;
-        this.programName = programName;
-        this.partVendor = partVendor;
-    }
-
+    /**
+     * @return the programNo
+     */
     public int getProgramNo() {
         return programNo;
     }
 
+    /**
+     * @param programNo the programNo to set
+     */
     public void setProgramNo(int programNo) {
         this.programNo = programNo;
     }
 
-    public int getPartNo() {
-        return partNo;
-    }
-
-    public void setPartNo(int partNo) {
-        this.partNo = partNo;
-    }
-
-    public int getPartInvent() {
-        return partInvent;
-    }
-
-    public void setPartInvent(int partInvent) {
-        this.partInvent = partInvent;
-    }
-
+    /**
+     * @return the programName
+     */
     public String getProgramName() {
         return programName;
     }
 
+    /**
+     * @param programName the programName to set
+     */
     public void setProgramName(String programName) {
         this.programName = programName;
     }
 
-    public String getPartVendor() {
-        return partVendor;
+    /**
+     * @return the partNo
+     */
+    public int getPartNo() {
+        return partNo;
     }
 
-    public void setPartVendor(String partVendor) {
-        this.partVendor = partVendor;
+    /**
+     * @param partNo the partNo to set
+     */
+    public void setPartNo(int partNo) {
+        this.partNo = partNo;
+    }
+
+    /**
+     * @return the inventory
+     */
+    public int getInventory() {
+        return inventory;
+    }
+
+    /**
+     * @param inventory the inventory to set
+     */
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    /**
+     * @return the vendor
+     */
+    public String getVendor() {
+        return vendor;
+    }
+
+    /**
+     * @param vendor the vendor to set
+     */
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
     }
     
 }
