a53038a99262458751420e443711502643b7774f
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-04-12 01:07:34 -0400

    Login and Mainframe updated

--- ECAM Group Project/src/View/ECAMView.java
+++ ECAM Group Project/src/View/ECAMView.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
 import java.util.HashMap;
 
 /**
- *
- * @author RIVER
+ * Purpose: Used to display LoginFrame on the computer screen
  */
 public class ECAMView {
     
